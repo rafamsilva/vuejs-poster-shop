308b2a7242f79f82abb62bab20366e59f8c80e15
Rafael <rafamsilva@live.com>
2017-05-07 19:19:46 -0300

    itens do carrinho no template

--- public/script.js
+++ public/script.js
@@ -7,13 +7,12 @@ new Vue({
             {title: 'Item 2'},
             {title: 'Item 3'}
         ],
-        cart: [
-
-        ]
+        cart: []
     },
     methods: {
         addItem: function(index) {
             this.total += 9.99;
+            this.cart.push(this.items[index]);
         }
     }
 });
